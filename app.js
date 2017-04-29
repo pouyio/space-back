e0c47b695be0f5cf696166e08fa52738fe8b10c6
Jorge Azorin <jorgeazorin@gmail.com>
2017-04-29 13:58:37 +0200

    token repair

--- app.js
+++ app.js
@@ -17,21 +17,23 @@ var app = express();
 
 var httpServer = http.createServer(app);
 var httpsServer = https.createServer(credentials, app);
-const fileUpload = require('express-fileupload');
-
+var multer  = require('multer')
+var upload = multer({ dest: './' })
 
 app.set('superSecret', 'hoooooooooolaJajajajja+-*/54'); // secret variable
 
 
 
 //app.use(morgan('dev'));
-app.use(fileUpload());
 
-app.post('/upload', function(req, res) {
-  if (!req.files)
-    return res.status(400).send('No files were uploaded.');
-  console.log(req.files);
-});
+app.post('/upload/', upload.single('file'), function (req, res, next) {
+  console.log(req.file);
+  res.send("Ok");
+
+  // req.file is the `avatar` file
+  // req.body will hold the text fields, if there were any
+})
+
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
--- artic-app/app.js
+++ artic-app/app.js
@@ -7,7 +7,7 @@ var controllers   =require('./controllers/_controllers.js');
 
 router.use(function(req, res, next) {
   res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, x-srf-token");
   next();
 });
 
