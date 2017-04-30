265d30b005eb7c19dc6bbc5d88862a4d3399f95f
Jorge Azorin <jorgeazorin@gmail.com>
2017-04-30 09:29:59 +0200

    tal

--- artic-app/bd/valorationCommands.js
+++ artic-app/bd/valorationCommands.js
@@ -7,7 +7,7 @@ var query = `SELECT challenge.name, challenge.description, participation.id part
 left join space_app.participation participation on participation.challenge = challenge.id
 left join space_app.user user on participation.user = user.id
 left join space_app.valoration valoration on valoration.participation = participation.id
-where participation.id is not null and  (select count(1) from space_app.valoration v where v.user = ? and v.participation != participation.id ) <1 and participation.user != ?
+where participation.id is not null and  (select count(1) from space_app.valoration v where v.user = ? and v.participation = participation.id ) <1 and participation.user != ?
 group by challenge.id, participation.id
 order by count(valoration.id);`
 
